123439978d75175da8ae1df50f37da5a3431635b
slarge <largesi@gmail.com>
2016-11-23 09:14:38 +0100

    add list of colors

---
+++ R/colList.R
@@ -0,0 +1,27 @@
+# COMPOUND 1
+colList1 <- c("#002D60", # ICES Blue: 
+              "#3D6899", # Dust Blue: 
+              "#00FEFF", # Bright Blue: 
+              "#FF6B40", # Pale Orange: 
+              "#CC2314") # Carmine Red: 
+
+# COMPOUND 2
+colList2 <- c("#80AEA0", # ICES Green: 
+              "#3D9970", # Olive Green:
+              "#00FF21", # Bright Green: 
+              "#FF40D3", # Fuchsia: 
+              "#B114CC") # Purple: 
+
+# COMPOUND 3
+colList3 <- c("#35C9B6", # Ocean Green: 
+              "#3D998D", # Ph. Green: 
+              "#00FF4E", # Bright Green 2:
+              "#FF4098", # Pink: 
+              "#CC14C5") # Fuchsia 2: 
+
+# COMPOUND 4: 
+colList4 <- c("#26FFFF", # Sky Blue: 
+              "#3D9999", # Dust Blue: 
+              "#00FF6C", # Bright Green 3: 
+              "#FF406C", # Dark pink:
+              "#CC149b") # Fuchsia 3: 

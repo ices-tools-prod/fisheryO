fdbd83f20c99c2ff5346e06234f4e739d044569a
Scott Large <scott.large@ices.local>
2017-05-10 15:19:29 +0200

    remove analytical code from vignette

--- R/fo_maps.R
+++ R/fo_maps.R
@@ -7,9 +7,11 @@ library(dplyr)
 
 ices_url <- "http://gis.ices.dk/shapefiles/ICES_areas.zip"
 eco_url <- "http://gis.ices.dk/shapefiles/ICES_ecoregions.zip"
-eu_url <- "http://biogeo.ucdavis.edu/data/gadm2.8/gadm28.shp.zip"
+# eu_url <- "http://biogeo.ucdavis.edu/data/gadm2.8/gadm28.shp.zip"
 
 tmp_path <- tempdir()
+eu_file <- "~/git/ices-dk/fisheryO/inst/extdata/EU_Country.zip"
+unzip(eu_file, exdir = tmp_path)
 
 get_map <- function(URL) {
   tmp_file <- tempfile(fileext = ".zip")
@@ -20,45 +22,34 @@ get_map <- function(URL) {
 }
 
 lapply(list(ices_url,
-            eco_url,
-            eu_url), get_map)
-
+            eco_url), get_map)
 
 ices_areas <- sf::st_read(dsn = tmp_path, layer = "ICES_Areas_20160601_dense", quiet = FALSE)
-eu_areas <- sf::st_read(dsn = tmp_path, layer = "gadm28", quiet = FALSE)
+eu_areas <- sf::st_read(dsn = tmp_path, layer = "EU_Country", quiet = FALSE)
 eco_areas <- sf::st_read(dsn = tmp_path, layer = "ICES_ecoregions_20150113_no_land", quiet = FALSE)
 
-library(dplyr)
-str(eu_areas)
+# Establish the CRS, let's use the Lambert Azimuthal Equal Area, centered at 52 N and 10 E
+eu_areas <- sf::st_transform(eu_areas, "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+
+colnames(ices_areas)
+colnames(eu_areas)
+colnames(eco_areas)
+
+st_bbox(ices_areas)
+
 eu_areas <- eu_areas %>%
   filter(CONTINENT == "Europe")
 
-  ggplot(eu_areas) +
-    geom_sf()
+eco_areas <- eco_areas %>%
+  filter(Ecoregion == "Greater North Sea")
 
-  # If not supplied, coord_sf() will take the CRS from the first layer
-  # and automatically transform all other layers to use that CRS. This
-  # ensures that all data will correctly line up
-  nc_3857 <- sf::st_transform(nc, "+init=epsg:3857")
-  ggplot() +
-    geom_sf(data = nc) +
-    geom_sf(data = nc_3857, colour = "red", fill = NA)
+ices_areas <- ices_areas %>%
+  filter(SubArea == "4")
 
-  # You can also use layers with x and y aesthetics: these are
-  # assumed to already be in the common CRS.
-  ggplot(nc) +
-    geom_sf() +
-    annotate("point", x = -80, y = 35, colour = "red", size = 4)
+ggplot() +
+  geom_sf(data = eu_areas) +
+  geom_sf(data = ices_areas, fill = "yellow", alpha = 0.2) +
+  geom_sf(data = eco_areas) +
+  coord_sf +
+  theme_bw()
 
-  # Thanks to the power of sf, ageom_sf nicely handles varying projections
-  # setting the aspect ratio correctly.
-  library(maps)
-  world1 <- sf::st_as_sf(map('world', plot = FALSE, fill = TRUE))
-  ggplot() + geom_sf(data = world1)
-
-  world2 <- sf::st_transform(
-    world1,
-    "+proj=laea +y_0=0 +lon_0=155 +lat_0=-90 +ellps=WGS84 +no_defs"
-  )
-  ggplot() + geom_sf(data = world2)
-# }

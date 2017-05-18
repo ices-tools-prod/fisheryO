25943f66f6883d1896a3ca6d7603b40afd6c37cc
Scott Large <slarge@users.noreply.github.com>
2017-05-18 14:33:03 +0200

    Closes #11

--- data-raw/load_raw_data.R
+++ data-raw/load_raw_data.R
@@ -25,25 +25,25 @@ sag_refpts_raw <- icesSAG::getSAG(stock = NULL,
 devtools::use_data(sag_refpts_raw)
 
 
-sag_keys <- do.call("rbind", lapply(2014:2016,
+sag_keys <- do.call("rbind", lapply(2014:2017,
                                     function(x) icesSAG::findAssessmentKey(stock = NULL,
                                                                            year = x,
                                                                            full = TRUE)[, c("AssessmentYear",
                                                                                             "AssessmentKey",
                                                                                             "StockKeyLabel")]))
-devtools::use_data(sag_keys)
+devtools::use_data(sag_keys_raw)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # DATA SOURCE: ICES official catch statistics (1950-2010) #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 historicURL <- "http://ices.dk/marine-data/Documents/CatchStats/HistoricalLandings1950-2010.zip"
 tmpFileHistoric <- tempfile(fileext = ".zip")
 download.file(historicURL, destfile = tmpFileHistoric, mode = "wb", quiet = TRUE)
-catch_data_historical <- read.csv(unz(tmpFileHistoric, "ICES_1950-2010.csv"),
+ices_catch_historical_raw <- read.csv(unz(tmpFileHistoric, "ICES_1950-2010.csv"),
                                   stringsAsFactors = FALSE,
                                   header = TRUE,
                                   fill = TRUE,
                                   na.strings = c("...", "-", "ns", "."))
-devtools::use_data(catch_data_historical)
+devtools::use_data(ices_catch_historical_raw)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # DATA SOURCE: ICES official catch statistics (2006-2014) #
@@ -52,14 +52,14 @@ devtools::use_data(catch_data_historical)
 catchURL <- "http://ices.dk/marine-data/Documents/CatchStats/OfficialNominalCatches.zip"
 tmpFileCatch <- tempfile(fileext = ".zip")
 download.file(catchURL, destfile = tmpFileCatch, mode = "wb", quiet = TRUE)
-catch_data_official <- read.csv(unz(tmpFileCatch, "ICESCatchDataset2006-2014.csv"),
+ices_catch_official_raw <- read.csv(unz(tmpFileCatch, "ICESCatchDataset2006-2014.csv"),
                                 stringsAsFactors = FALSE,
                                 header = TRUE,
                                 fill = TRUE)
 # remove columns with all NA
-catch_data_official <- Filter(function(x)!all(is.na(x)), catch_data_official)
+ices_catch_official_raw <- Filter(function(x)!all(is.na(x)), ices_catch_official_raw)
 
-devtools::use_data(catch_data_official)
+devtools::use_data(ices_catch_official_raw)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # DATA SOURCE: FAO species names and labels #
@@ -68,20 +68,20 @@ devtools::use_data(catch_data_official)
 spURL <- "ftp://ftp.fao.org/FI/STAT/DATA/ASFIS_sp.zip"
 tmpFileSp <- tempfile(fileext = ".zip")
 download.file(spURL, destfile = tmpFileSp, mode = "wb", quiet = TRUE)
-species_list <- read.delim("~/git/ices-dk/fisheryO/inst/extdata/ASFIS_sp_Feb_2016.txt",
+species_list_raw <- read.delim("~/git/ices-dk/fisheryO/inst/extdata/ASFIS_sp_Feb_2016.txt",
                            fill = TRUE,
                            stringsAsFactors = FALSE,
                            header = TRUE,
                            na.strings = "")
-devtools::use_data(species_list, overwrite = TRUE)
+devtools::use_data(species_list_raw)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # DATA SOURCE: STECF Effort and Catch tables #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 STECF_effort_data <- readRDS("data-raw/STECF_effort_data.rds")
-devtools::use_data(STECF_effort_data, overwrite = TRUE)
+devtools::use_data(STECF_effort_data)
 
 STECF_landings_data <- readRDS("data-raw/STECF_landings_data.rds")
-devtools::use_data(STECF_landings_data, overwrite = TRUE)
+devtools::use_data(STECF_landings_data)
 

78fadd295d5179ae23d0aa6ce0e8aadae53a1525
Scott Large <slarge@users.noreply.github.com>
2017-06-29 12:43:49 +0200

    add bit to define hyperlinks for most recent advice. I guess it can close #4

--- analysis/baltic_FO.R
+++ analysis/baltic_FO.R
@@ -1,9 +1,10 @@
-# library(fisheryO)
+devtools::install_github("slarge/fisheryO")
+
+library(fisheryO)
 rm(list = ls())
 ecoregion = "Baltic Sea Ecoregion"
 active_year = 2017
-calculate_status = FALSE
-fisheries_guild = c("benthic", "demersal", "pelagic")
+output_path <- "~/git/ices-dk/fisheryO/output/"
 
 ## Figure 1.
 area_definition_map(ecoregion,
@@ -50,9 +51,9 @@ ices_catch_plot(ecoregion,
                 line_count = 5,
                 plot_type = "line",
                 file_name = "baltic_figure4",
-                save_plot = FALSE,
-                output_path = "output/",
-                return_plot = TRUE,
+                save_plot = TRUE,
+                output_path = output_path,
+                return_plot = FALSE,
                 fig.width = 174,
                 fig.height = 68,
                 text.size = 9)
@@ -124,7 +125,7 @@ stockPie_fun(ecoregion,
              active_year = 2017,
              save_plot = TRUE,
              return_plot = FALSE,
-             output_path = "output/")
+             output_path = output_path)
 
 ## Figure 11.
 gesPie_fun(ecoregion,
@@ -135,7 +136,7 @@ gesPie_fun(ecoregion,
            active_year = 2017,
            save_plot = TRUE,
            return_plot = FALSE,
-           output_path = "output/")
+           output_path = output_path)
 
 ## Figure 12.
 stock_trends_fun(EcoGuild = "Baltic Sea Ecoregion - benthic stocks",
@@ -143,27 +144,27 @@ stock_trends_fun(EcoGuild = "Baltic Sea Ecoregion - benthic stocks",
                  dynamic = FALSE,
                  data_caption = FALSE,
                  file_name = "baltic_figure12_benthic",
-                 save_plot = FALSE,
-                 return_plot = TRUE,
-                 output_path = "output/")
+                 save_plot = TRUE,
+                 return_plot = FALSE,
+                 output_path = output_path)
 
 stock_trends_fun(EcoGuild = "Baltic Sea Ecoregion - demersal stocks",
                  active_year = 2017,
                  dynamic = FALSE,
                  data_caption = FALSE,
                  file_name = "baltic_figure12_demersal",
-                 save_plot = FALSE,
-                 return_plot = TRUE,
-                 output_path = "output/")
+                 save_plot = TRUE,
+                 return_plot = FALSE,
+                 output_path = output_path)
 
 stock_trends_fun(EcoGuild = "Baltic Sea Ecoregion - pelagic stocks",
                  active_year = 2017,
                  dynamic = FALSE,
                  data_caption = FALSE,
                  file_name = "baltic_figure12_pelagic",
-                 save_plot = FALSE,
-                 return_plot = TRUE,
-                 output_path = "output/")
+                 save_plot = TRUE,
+                 return_plot = FALSE,
+                 output_path = output_path)
 
 ## Figure 13.
 plot_kobe(ecoregion,
@@ -171,40 +172,55 @@ plot_kobe(ecoregion,
           active_year = 2017,
           data_caption = FALSE,
           file_name = "baltic_figure13_all",
-          output_path = "output/",
-          return_plot = TRUE,
-          save_plot = FALSE)
+          output_path = output_path,
+          return_plot = FALSE,
+          save_plot = TRUE)
 
 plot_kobe(ecoregion,
           guild = "benthic",
           active_year = 2017,
           data_caption = FALSE,
           file_name = "baltic_figure13_benthic",
-          output_path = "output/",
-          return_plot = TRUE,
-          save_plot = FALSE)
+          output_path = output_path,
+          return_plot = FALSE,
+          save_plot = TRUE)
 
 plot_kobe(ecoregion,
           guild = "pelagic",
           active_year = 2017,
           data_caption = FALSE,
           file_name = "baltic_figure13_pelagic",
-          output_path = "output/",
+          output_path = output_path,
           return_plot = FALSE,
           save_plot = TRUE)
+
 
 plot_kobe(ecoregion,
           guild = "demersal",
           active_year = 2017,
           data_caption = FALSE,
           file_name = "baltic_figure13_demersal",
-          output_path = "output/",
+          output_path = output_path,
           return_plot = FALSE,
           save_plot = TRUE)
 
-stockSummaryTable_fun(ecoregion,
-                      active_year = 2017,
-                      table_type = "static_docx",
-                      output_path = "output/",
-                      file_name = "baltic_annex")
+# 
+# stockSummaryTable_fun(ecoregion,
+#                       active_year = 2017,
+#                       table_type = "static_docx",
+#                       output_path = output_path,
+#                       file_name = "baltic_annex")
 
+dat <- clean_stock_trends(active_year)
+clicks <- dat$sag_complete_summary %>%
+  tidyr::unnest() %>% 
+  filter(EcoRegion == "Baltic Sea Ecoregion") %>% 
+  select(StockCode,
+         YearOfLastAssessment) %>% 
+  distinct(.keep_all = TRUE) %>% 
+  mutate(onclick = sprintf("%s%i/%i/%s.pdf",
+                           "http://ices.dk/sites/pub/Publication%20Reports/Advice/",
+                           YearOfLastAssessment,
+                           YearOfLastAssessment,
+                           StockCode)) %>% 
+  write.csv(file = paste0(output_path, "baltic_links.csv"), row.names = FALSE)
